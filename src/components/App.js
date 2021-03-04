ad7d25314d181b1bf20f2c83310830f6d7162245
daniel <daniel.obeng@macaulay.cuny.edu>
2021-03-03 23:22:23 -0500

    react starter app

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { ReactComponent as Logo } from 'assets/logo.svg';
+import 'styles/App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo className="App-logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
