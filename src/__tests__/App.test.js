fc3c61b7cfbe7928656152a7cb35f21d54853416
daniel <daniel.obeng@macaulay.cuny.edu>
2021-03-04 00:34:02 -0500

    adds tests for solution

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from 'components/App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// two-kinds of testing - snapshot testing, and DOM testing
+
+describe('Test App', () => {
+  // snapshot testing
+  it('matches home pages snapshot', () => {
+    const { asFragment } = render(<App />);
+    expect(asFragment()).toMatchSnapshot();
+  })
+
+  // DOM testing -> render components and test them
+  // testing library is different from common libraries
+  //  The utilities this library provides facilitate querying the DOM in the same way the user would. Finding form elements by their label text (just like a user would),
+  // finding links and buttons from their text (like a user would).
+  it('toggles text correctly', () => {
+    const { getByRole, getByText } = render(<App />);
+    expect(getByRole('heading', { level: 1})).toHaveTextContent(/This is React/i) // i = case-insensitive
+
+    fireEvent.click(getByRole('button'))
+
+    const headerElement = getByText(/This is CSC 59939/i)
+    expect(headerElement).toBeInTheDocument();
+  })
+})
---
+++ src/utils.js
@@ -0,0 +1,5 @@
+export const add = (a, b) => {
+    const result = `${ a } ${ b }`;
+    // alert(result);
+    return result;
+}
